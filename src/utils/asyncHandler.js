567686880bb00a01d49e7dedf6422c377055d4d1
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2024-08-22 22:27:03 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHolder = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+// export { asyncHolder };
+// const asyncHolder = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({ success: false, message: err.message });
+//   }
+// };
