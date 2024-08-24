53c68dc18a66f4171fc18da3ce4e06f97df15064
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2024-08-24 16:54:06 +0530

    successfully registered a user

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
     this.data = null;
     this.message = message;
     this.success = false;
-    this.errors = errors;
+    this.errors = error; // Fix this line
     if (stack) {
       this.stack = stack;
     } else {
