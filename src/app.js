aa9bf5c5c7214eb12b66e22e5d2b499871b8abc7
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2024-08-24 12:21:05 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,5 +12,9 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser);
+app.use(cookieParser());
+// importing routes
+import userRouter from "./routes/user.routes.js";
+// router declaration
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
-const asyncHolder = (requestHandler) => {
-  (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
@@ -11,3 +11,4 @@ const asyncHolder = (requestHandler) => {
 //     res.status(err.code || 500).json({ success: false, message: err.message });
 //   }
 // };
+export { asyncHandler };
